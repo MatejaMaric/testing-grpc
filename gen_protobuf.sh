4176e0261ef93f3fb43498ea77ba7b17d4e319ff
Mateja <mail@matejamaric.com>
2023-09-10 12:07:03 +0200

    Nicer file structure

--- gen_protobuf.sh
+++ gen_protobuf.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# protoc --go_out=. pb/test_obj.proto
-
-protoc -I=pkg/pb \
+protoc -I=proto \
   --go_out=paths=source_relative:pkg/pb \
   --go-grpc_out=paths=source_relative:pkg/pb \
-  pkg/pb/*.proto
+  proto/*.proto
